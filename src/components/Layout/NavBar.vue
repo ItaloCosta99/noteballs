e2c571aaa9a93190ff1e24d4a23ebd6c61408768
Italo Costa <italocostajobs@gmail.com>
2024-07-26 17:01:08 -0300

    Design with bulma

---
+++ src/components/Layout/NavBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="navbar is-success" role="navigation" aria-label="main navigation">
+    <div class="container is-max-desktop px-2">
+      <div class="navbar-brand">
+        <div class="navbar-item is-size-4 is-family-monospace">Noteballs</div>
+
+        <a
+          @click.prevent="showBurgerMenu = !showBurgerMenu"
+          role="button"
+          class="navbar-burger"
+          :class="{ 'is-active': showBurgerMenu }"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu" :class="{ 'is-active': showBurgerMenu }">
+        <div class="navbar-end">
+          <RouterLink to="/" class="navbar-item"> Notes </RouterLink>
+
+          <RouterLink to="/stats" class="navbar-item"> Stats </RouterLink>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+/* imports */
+import { ref } from 'vue'
+
+/* nav */
+const showBurgerMenu = ref(false)
+</script>
+
+<style>
+@media screen and (max-width: 1023px) {
+  .navbar-menu {
+    position: absolute;
+    left: 0;
+    width: 100%;
+  }
+}
+</style>
